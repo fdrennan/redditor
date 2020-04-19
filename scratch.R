7237fadb45019f2e5f1853982279a736e67a6602
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-18 23:35:19 -0600

    update

--- scratch.R
+++ scratch.R
@@ -8,14 +8,20 @@ reddit_con = praw$Reddit(client_id=Sys.getenv('REDDIT_CLIENT'),
                          username=Sys.getenv('USERNAME'),
                          password=Sys.getenv('PASSWORD'))
 
+str_detect_any <- function(string = NULL, pattern = NULL) {
+  any(str_detect(string = str_to_lower(string), pattern = pattern))
+}
 politics <- reddit_con$subreddit('all')
 iterate(politics$stream$comments(), function(x) {
-  # if (x$is_submitter) {
+  if(str_detect_any(x$body,
+                    c('trump', 'socialism', 'conservatism', 'biden'))) {
     message(
       glue("\n\n{x$author}\n{x$body}\nIs Submitter: {x$is_submitter}\nLink: {x$link_permalink}")
     )
-  # }
+  }
 })
+
+
 
 
 
